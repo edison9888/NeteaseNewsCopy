4c6ddd303c7d7ebd67567911e805e6cb5495f267
AkiraX <cw.akira@gmail.com>
2014-03-18 16:17:54 +0800

    123
    test

---
+++ GitCodeTest/GitCodeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitCodeTest
+//
+//  Created by TY on 14-3-18.
+//  Copyright (c) 2014年 NERV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitCodeTest/GitCodeTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitCodeTest
+//
+//  Created by TY on 14-3-18.
+//  Copyright (c) 2014年 NERV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
